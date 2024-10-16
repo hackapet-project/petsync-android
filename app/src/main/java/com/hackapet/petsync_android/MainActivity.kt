4c323601fbe5469d36f008a5bf6a5f489fe0c099
xenione <xenitwo@gmail.com>
2024-10-16 13:39:27 +0200

    Added kmp library

--- app/src/main/java/com/hackapet/petsync_android/MainActivity.kt
+++ app/src/main/java/com/hackapet/petsync_android/MainActivity.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hackapet.petsync_android.ui.theme.PetsyncandroidTheme
+import com.hackapet.petsync_kmp.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +24,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PetsyncandroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val greetings by remember { mutableStateOf(Greeting().greet()) }
                     Greeting(
-                        name = "Android",
+                        name = greetings,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +38,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = name,
         modifier = modifier
     )
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        mavenLocal()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -14,6 +15,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
         google()
         mavenCentral()
     }
